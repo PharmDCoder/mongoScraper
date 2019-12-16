af9212f6147d212f7966fe39e985903c9d7a5ee0
Jamie ONeill <joneill@sembracare.com>
2019-12-16 12:34:25 -0500

    adding save article function w/ html page & routes; moved notes from scraped to save articles

--- public/app.js
+++ public/app.js
@@ -16,73 +16,41 @@ $(".clear").on("click", () => {
 
 // Grab the articles as a json
 $.getJSON("/articles", function (data) {
+  $("#articles").empty();
   // For each one
-  for (var i = 0; i < data.length; i++) {
-    // Display the apropos information on the page
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
-  }
-});
-
-
-// Whenever someone clicks a p tag
-$(document).on("click", "p", function () {
-  // Empty the notes from the note section
-  $("#notes").empty();
-  // Save the id from the p tag
-  var thisId = $(this).attr("data-id");
-
-  // Now make an ajax call for the Article
-  $.ajax({
-    method: "GET",
-    url: "/articles/" + thisId
-  })
-    // With that done, add the note information to the page
-    .then(function (data) {
-      console.log(data);
-      // The title of the article
-      $("#notes").append("<h2>" + data.title + "</h2>");
-      // An input to enter a new title
-      $("#notes").append("<input id='titleinput' name='title' >");
-      // A textarea to add a new note body
-      $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-      // A button to submit a new note, with the id of the article saved to it
-      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-
-      // If there's a note in the article
-      if (data.note) {
-        // Place the title of the note in the title input
-        $("#titleinput").val(data.note.title);
-        // Place the body of the note in the body textarea
-        $("#bodyinput").val(data.note.body);
-      }
-    });
-});
-
-// When you click the savenote button
-$(document).on("click", "#savenote", function () {
-  // Grab the id associated with the article from the submit button
-  var thisId = $(this).attr("data-id");
-
-  // Run a POST request to change the note, using what's entered in the inputs
-  $.ajax({
-    method: "POST",
-    url: "/articles/" + thisId,
-    data: {
-      // Value taken from title input
-      title: $("#titleinput").val(),
-      // Value taken from note textarea
-      body: $("#bodyinput").val()
+  var titlesArr = [];
+  $.getJSON("/getSaved", function (savedData) {
+    // For each one
+    for (var i = 0; i < savedData.length; i++) {
+      titlesArr.push(savedData[i].title);
     }
-  })
-    // With that done
-    .then(function (data) {
-      // Log the response
-      console.log(data);
-      // Empty the notes section
-      $("#notes").empty();
-    });
+  }).then(() => {
+    for (var i = 0; i < data.length; i++) {
+      if ((titlesArr.indexOf(data[i].title.replace("'", "")) === -1)) {
+        var newDiv = $("<div>");
+        newDiv.append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "<br />" + data[i].date + "</p>");
+        newDiv.append("<button class='btn btn-primary save-click' data-title='" + data[i].title.replace("'", "") + "' data-link='"
+          + data[i].link + "' data-date='" + data[i].date + "'>Save</button>")
+        // Display the apropos information on the page
+        $("#articles").append(newDiv);
+      }
+    }
+  });
 
-  // Also, remove the values entered in the input and textarea for note entry
-  $("#titleinput").val("");
-  $("#bodyinput").val("");
 });
+
+$(document).on("click", ".save-click", function () {
+
+  var result = {};
+
+  // Add the text and href of every link, and save them as properties of the result object
+  result.title = $(this).data("title");
+  result.link = $(this).data("link");
+  result.date = $(this).data("date");
+  $(this).parent().hide();
+  $.post("/save-article", result, function (req, res) {
+  }).then(() => {
+  })
+});
+
+
--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
-
+var path = require("path");
 // Configure middleware
 
 // Use morgan logger for logging requests
@@ -34,32 +34,29 @@ mongoose.connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true 
 // Routes
 
 // A GET route for scraping the echoJS website
-app.get("/scrape", function(req, res) {
+app.get("/scrape", function (req, res) {
   // First, we grab the body of the html with axios
-  axios.get("http://www.echojs.com/").then(function(response) {
+  axios.get("https://www.pbs.org/wgbh/frontline/").then(function (response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
     // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
+    $(".the-latest h6").each(function (i, element) {
       // Save an empty result object
       var result = {};
 
       // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this)
-        .children("a")
-        .text();
-      result.link = $(this)
-        .children("a")
-        .attr("href");
+      result.title = $(this).children("a").text().replace("'", "");
+      result.link = $(this).children("a").attr("href");
+      result.date = $(this).parent().children("div:last-child").text();
 
       // Create a new Article using the `result` object built from scraping
       db.Article.create(result)
-        .then(function(dbArticle) {
+        .then(function (dbArticle) {
           // View the added result in the console
           console.log(dbArticle);
         })
-        .catch(function(err) {
+        .catch(function (err) {
           // If an error occurred, log it
           console.log(err);
         });
@@ -70,64 +67,107 @@ app.get("/scrape", function(req, res) {
   });
 });
 
+app.post("/save-article", function (req, res) {
+  console.log(req.body);
+  // Create a new Article using the `result` object built from scraping
+  db.Saved.create(req.body)
+    .then(function (dbSaved) {
+      // View the added result in the console
+      console.log(dbSaved);
+    })
+    .catch(function (err) {
+      // If an error occurred, log it
+      console.log(err);
+    });
+});
+
 //Remove Articles
 app.delete('/clear', function (req, res) {
   // res.send('Got a DELETE request at /user')
-  db.Article.remove({}, function(error, removed) {
+  
+  db.Article.remove({}, function (error, removed) {
   });
 })
 
+app.delete("/deleteSaved/:id", function(req, res) {
+  // We just have to specify which todo we want to destroy with "where"
+  db.Saved.findByIdAndRemove(req.params.id, function (error, removed) {
+  }).then(function(dbSaved) {
+    res.json(dbSaved);
+  });
+});
+
 // Route for getting all Articles from the db
-app.get("/articles", function(req, res) {
+app.get("/articles", function (req, res) {
   // Grab every document in the Articles collection
   db.Article.find({})
-    .then(function(dbArticle) {
+    .then(function (dbArticle) {
       // If we were able to successfully find Articles, send them back to the client
       res.json(dbArticle);
     })
-    .catch(function(err) {
+    .catch(function (err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Route for getting all Articles from the db
+app.get("/saved", function (req, res) {
+
+  res.sendFile(path.join(__dirname, "public/saved.html"));
+});
+
+app.get("/getSaved", function (req, res) {
+
+  // Grab every document in the Articles collection
+  db.Saved.find({})
+    .then(function (dbSaved) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.json(dbSaved);
+    })
+    .catch(function (err) {
       // If an error occurred, send it to the client
       res.json(err);
     });
 });
 
 // Route for grabbing a specific Article by id, populate it with it's note
-app.get("/articles/:id", function(req, res) {
+app.get("/articles/:id", function (req, res) {
   // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-  db.Article.findOne({ _id: req.params.id })
+  db.Saved.findOne({ _id: req.params.id })
     // ..and populate all of the notes associated with it
     .populate("note")
-    .then(function(dbArticle) {
+    .then(function (dbSaved) {
       // If we were able to successfully find an Article with the given id, send it back to the client
-      res.json(dbArticle);
+      res.json(dbSaved);
     })
-    .catch(function(err) {
+    .catch(function (err) {
       // If an error occurred, send it to the client
       res.json(err);
     });
 });
 
 // Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function(req, res) {
+app.post("/articles/:id", function (req, res) {
   // Create a new note and pass the req.body to the entry
   db.Note.create(req.body)
-    .then(function(dbNote) {
+    .then(function (dbNote) {
       // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
       // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
       // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+      return db.Saved.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
     })
-    .then(function(dbArticle) {
+    .then(function (dbSaved) {
       // If we were able to successfully update an Article, send it back to the client
-      res.json(dbArticle);
+      res.json(dbSaved);
     })
-    .catch(function(err) {
+    .catch(function (err) {
       // If an error occurred, send it to the client
       res.json(err);
     });
 });
 
 // Start the server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App running on port " + PORT + "!");
 });
